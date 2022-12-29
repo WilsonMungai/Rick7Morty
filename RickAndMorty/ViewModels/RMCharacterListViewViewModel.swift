69a806286ed01aa73689ec15a4ac7ab937c5ca03
WilsonMungai <wilsonmuguthi@gmail.com>
2022-12-29 11:10:12 -0500

    Loading data onto the cell content view

--- RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
@@ -15,8 +15,7 @@ final class RMCharacterListViewViewModel: NSObject
             switch result
             {
             case.success(let model):
-                print("Total: "+String(model.info.pages))
-                print("Page result count: "+String(model.results.count))
+                print("Example image url: "+String(model.results.first?.image ?? "No image"))
             case .failure(let error):
                 print(String(describing: error))
             }
@@ -33,8 +32,18 @@ extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollection
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .systemRed
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier, for: indexPath
+        )as? RMCharacterCollectionViewCell else{
+            fatalError("Unsupported Cell")
+        }
+        let viewModel = RMCharacterCollectionViewCellViewModel(
+            characterName: "Wilson",
+            characterStatus: .alive,
+            characterImageUrl: URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"))
+        
+        cell.configure(with: viewModel)
+       
         return cell
     }
     
