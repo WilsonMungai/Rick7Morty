379e37351e39aaddaba0c4551390c93754cd1853
WilsonMungai <wilsonmuguthi@gmail.com>
2023-01-03 18:10:49 -0500

    image caching

---
+++ RickAndMorty/Managers/RMImageLoader.swift
@@ -0,0 +1,46 @@
+//
+//  ImageLoader.swift
+//  RickAndMorty
+//
+//  Created by Wilson Mungai on 2023-01-03.
+//
+
+import Foundation
+
+final class RMImageLoader
+{
+    static let shared = RMImageLoader()
+    
+    // Handles getting rid of session in the cache incase memory is getting low
+    private var imageDataCache = NSCache<NSString, NSData>()
+    
+    private init(){}
+    
+    /// Get image data with url
+    /// - Parameters:
+    ///  - url: Source url
+    ///  - completion: Callback
+    public func downLoadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void)
+    {
+        let key = url.absoluteString as NSString
+        if let data = imageDataCache.object(forKey: key)
+        {
+            completion(.success(data as Data))
+            return
+        }
+        let request = URLRequest(url:url)
+        let task = URLSession.shared.dataTask(with: request)
+        { [weak self] data, _, error in
+            guard let data = data, error == nil
+            else
+            {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            let value = data as NSData
+            self?.imageDataCache.setObject(value, forKey: key)
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
