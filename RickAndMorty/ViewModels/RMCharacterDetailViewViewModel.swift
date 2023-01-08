848aa080dbcbddfe521b1400af800b672be57eff
WilsonMungai <wilsonmuguthi@gmail.com>
2023-01-08 10:36:28 -0500

    More setup on the character detail screen compositional layout

--- RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 final class RMCharacterDetailViewViewModel
 {
@@ -39,4 +40,66 @@ final class RMCharacterDetailViewViewModel
         character.name.uppercased()
     }
     
+    // MARK: - Character details Layouts
+    
+    // Character photo layout section
+    public func createPhotoSectionLayout() -> NSCollectionLayoutSection
+    {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(1.0)))
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0)
+        
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(0.5)
+            ),
+            subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    // Character information layout section
+    public func createInformationSectionLayout() -> NSCollectionLayoutSection
+    {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
+                                               heightDimension: .fractionalHeight(1.0)))
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(150)
+            ),
+            subitems: [item, item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    // Character episode layout section
+    public func createEpisodeSectionLayout() -> NSCollectionLayoutSection
+    {
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(1.0)))
+        
+        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 5, bottom: 10, trailing: 8)
+        
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(0.8),
+                heightDimension: .absolute(150)
+            ),
+            subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        // The section will scroll in relation to the main layout axis
+        // paging method snaps the next page into place, continuous method scrolling is non stop
+        section.orthogonalScrollingBehavior = .paging
+        return section
+    }
 }
